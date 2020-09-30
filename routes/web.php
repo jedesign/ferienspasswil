e6480278a0bb7f2fa01437d6fc05318e6733a6a0
Marco Rieser <marco.rieser@jedesign.ch>
2020-09-30 18:14:53 +0200

    Restore initial route

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//    $user =     \App\Models\User::first();
-//    dd($user->guardian()->toSql());
-//    dd($user->employee()->toSql());
-//    dd($user->fullname.', '.$user->guardian->address);
-//    dd(\App\Models\User::find(1)->guardian->participants->first()->firstname);
     return view('welcome');
 });
