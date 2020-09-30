11e26359efd162dd2ab7ffc743eeb26e00f33d60
Marco Rieser <marco.rieser@jedesign.ch>
2020-09-30 21:59:43 +0200

    Break tests to check the github actions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Course;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    abort(403);
 });
