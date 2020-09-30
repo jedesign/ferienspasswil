ef0b3c83034e2a3862815f57154b55255fe0325a
Marco Rieser <marco.rieser@jedesign.ch>
2020-09-30 21:18:42 +0200

    Write Unit Tests for Employee Model

---
+++ tests/Unit/EmployeeTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Employee;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class EmployeeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_has_an_id()
+    {
+        /** @var Employee $employee */
+        $employee = Employee::factory()->create();
+        $this->assertIsInt($employee->id);
+    }
+
+    /** @test */
+    public function it_can_have_a_public_transport_subscription()
+    {
+        /** @var Employee $employee */
+        $employee = Employee::factory(['public_transport_subscription' => null])->create();
+        $this->assertNull($employee->public_transport_subscription);
+
+        $employee = Employee::factory(['public_transport_subscription' => 'GA'])->create();
+        $this->assertIsString($employee->public_transport_subscription);
+    }
+
+    /** @test */
+    public function it_belongs_to_a_user()
+    {
+        /** @var Employee $employee */
+        $employee = Employee::factory()->has(User::factory())->create();
+
+        $this->assertInstanceOf(User::class, $employee->user);
+    }
+}
