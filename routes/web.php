afb261d6459541d8e182cf6729962bb4bad05c95
Marco Rieser <marco.rieser@jedesign.ch>
2020-09-30 22:01:35 +0200

    Back to working tests

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Course;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    abort(403);
+    return view('welcome');
 });
