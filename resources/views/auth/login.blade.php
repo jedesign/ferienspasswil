ed7186eb3d7761b1f6b0beb32ea7f2e832bb1d37
Marco Rieser <marco.rieser@jedesign.ch>
2020-10-09 18:05:01 +0200

    Login View

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,97 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+@section('title', 'Sign in to your account')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div>
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <a href="{{ route('home') }}">
+                <x-logo class="w-auto h-16 mx-auto text-indigo-600"/>
+            </a>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+            <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900 leading-9">
+                {{ __('Sign in to your account') }}
+            </h2>
+            <p class="mt-2 text-sm text-center text-gray-600 leading-5 max-w">
+                Or
+                <a href="{{ route('register') }}"
+                   class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                    create a new account
+                </a>
+            </p>
+        </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div class="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div>
+                        <label for="email" class="block text-sm font-medium text-gray-700 leading-5">
+                            {{ __('E-Mail Address') }}
+                        </label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <input autocomplete="email"
+                                   autofocus
+                                   class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('email') border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red @enderror"
+                                   id="email"
+                                   name="email"
+                                   required
+                                   type="email"
+                                   value="{{ old('email') }}"/>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        @error('email')
+                        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    <div class="mt-6">
+                        <label for="password"
+                               class="block text-sm font-medium text-gray-700 leading-5">
+                            {{ __('Password') }}
+                        </label>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <input autocomplete="current-password"
+                                   class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('password') border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red @enderror"
+                                   id="password"
+                                   name="password"
+                                   required
+                                   type="password"/>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        @error('password')
+                        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center">
+                            <input id="remember" type="checkbox" name="remember"
+                                   class="form-checkbox w-4 h-4 text-indigo-600 transition duration-150 ease-in-out" {{ old('remember') ? 'checked' : '' }}/>
+                            <label for="remember" class="block ml-2 text-sm text-gray-900 leading-5">
+                                {{ __('Remember Me') }}
+                            </label>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <div class="text-sm leading-5">
+                            <a href="{{ route('password.request') }}"
+                               class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
                         </div>
-                    </form>
-                </div>
+                    </div>
+
+                    <div class="mt-6">
+                    <span class="block w-full rounded-md shadow-sm">
+                        <button type="submit"
+                                class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                            Sign in
+                        </button>
+                    </span>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
 @endsection
