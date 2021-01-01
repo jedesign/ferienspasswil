74609c36afd5c53ab92b588b37e5b85b9f21eee9
Marco Rieser <marco.rieser@jedesign.ch>
2021-01-01 10:52:06 +0100

    Set autofocus on first input on login form

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -20,6 +20,7 @@
                     </label>
                     <div class="mt-1">
                         <input autocomplete="email"
+                               autofocus
                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                id="email"
                                name="email"
