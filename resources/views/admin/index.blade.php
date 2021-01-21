d25b9a1ca332090830a92a84a1bde8d6982822ff
Raphi W <ditshej@gmail.com>
2021-01-21 21:54:29 +0100

    employee 👷‍ dashboard: - add some view structure 🕋 - and some todos ⚒️ 😂

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,9 +5,12 @@
 @endsection
 
 @section('content')
-        {{dd($employee)}}
-{{--    <div class="col-span-6 lg:col-span-2 order-0 lg:order-1">--}}
-{{--        <h2 class="text-2xl font-bold leading-tight text-gray-700 mb-6">{{__('Your Data')}}</h2>--}}
-{{--        <x-guardian.card :guardian="$employee"/>--}}
-{{--    </div>--}}
+        <div class="col-span-6 lg:col-span-4 order-1 lg:order-0">
+            <h2 class="text-2xl font-bold leading-tight text-gray-700 mb-6">{{__('future stuff 🐟')}}</h2>
+            <!-- TODO: add stuff to manage (21.01.21 rw) -->
+        </div>
+        <div class="col-span-6 lg:col-span-2 order-0 lg:order-1">
+            <h2 class="text-2xl font-bold leading-tight text-gray-700 mb-6">{{__('Your Data')}}</h2>
+            <!-- TODO: add user-card with $employee (21.01.21 rw) -->
+        </div>
 @endsection
