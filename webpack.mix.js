e0ecfb6b09b2506600be0584769025b50ebe9694
Marco Rieser <marco.rieser@jedesign.ch>
2021-03-29 19:40:02 +0200

    This is the last time we let Dimitris commit ascii porn in the comments.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix');
 mix
     .js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
+        require('@tailwindcss/jit'),
     ])
     .browserSync({
         proxy: 'https://ferienspasswil.test',
