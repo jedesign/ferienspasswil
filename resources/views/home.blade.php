3cc35e77286970b67e2f718c5e800349cc4abc5c
Raphi W <ditshej@gmail.com>
2021-05-30 16:17:16 +0200

    add Course-Nav-Item on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
     <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 items-center">
+        <nav class="mb-5 max-w-md mx-auto sm:flex sm:justify-center md:mb-8">
+            <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+                <a href="{{ route('course.index') }}"
+                   class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+                >
+                    {{ __('Courses') }}
+                </a>
+            </div>
+        </nav>
         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">{{ config('app.name') }} @ {{config('app.env')}}</h1>
         @if (Route::has('login'))
             @auth
--- tests/Feature/Story/VisitorVisitsCoursePageTest.php
+++ tests/Feature/Story/VisitorVisitsCoursePageTest.php
@@ -15,7 +15,6 @@ class VisitorVisitsCoursePageTest extends TestCase
     /** @test */
     public function visitor_can_access_course_page_trough_home(): void
     {
-        self::markTestSkipped();
         $this->get(route('home'))->assertSee(route('course.index'));
     }
 
@@ -23,22 +22,6 @@ class VisitorVisitsCoursePageTest extends TestCase
     public function visitor_can_view_course_page(): void
     {
         $this->get(route('course.index'))->assertSuccessful();
-    }
-
-    /** @test */
-    // TODO[rw]: 🏋️‍ (20.05.21 rw)
-    public function visitor_can_view_course_overview(): void
-    {
-        self::markTestSkipped();
-        $this->get(route('course.index'))->assertSeeLivewire();
-    }
-
-    /** @test */
-    // TODO[rw]: 🏋️‍ (20.05.21 rw)
-    public function visitor_can_view_weather_status_of_weather_sensitive_course(): void
-    {
-        self::markTestSkipped();
-        $this->get(route('course.index'))->assertSee();
     }
 
 }
